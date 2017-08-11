14a2f24e064a79e369ad63b5507b267dd9d9eb54
Neoggg <thegitem@hotmail.com>
2017-08-11 18:34:07 +0800

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 //print_r($_POST);
-$dir_pwd="upload/up_img/";
+$dir_pwd="upload/live_up_img/liver_img_temporary/";
 if(isset($_POST['del_info'])){
     unlink($dir_pwd.$_POST['del_info']);
 
--- upload.php
+++ upload.php
@@ -1,18 +1,36 @@
 <?php
 //print_r($_POST);
 //print_r($_FILES);
-exit;
-$pwd="upload/up_img/";
-$time=Date('Y-m-d h:i:s');
-
-$num=1;
+//exit;
+$dir="liver_img_temporary";
+$pwd="upload/live_up_img/";
+//上傳檔案-----------------------------
+if(!file_exists($pwd.$dir)){
+ $oldmask = umask(0);
+ mkdir($pwd.$dir,0777);
+ umask($oldmask);
+}
 foreach ($_FILES["img_file"]["name"] as $key => $value) {
         
         $tmp_name = $_FILES["img_file"]["tmp_name"][$key];
-        $name = $pwd.$_FILES["img_file"]["name"][$key];
+
+        $name = $pwd.$dir."/".$_FILES["img_file"]["name"][$key];
         
         move_uploaded_file($tmp_name, $name);
     
 }
+//更改檔名----------------------------
+// $dh=opendir($dirname);
+// $i=1;
+// while ($dave=readdir($dh))
+// {
+// if ($dave != "." && $dave != "..") { 
+// 	$ex_name=pathinfo($dirname.$dave,PATHINFO_EXTENSION);
+// //echo $dave." ";
+// rename($dirname.$dave,$dirname.$num.$i.".".$ex_name);
+// $i++;
+// } 
+// }
+// closedir ($dh);
 
 ?>
